36d861a71d0af0c637770840549adaddae6d82de
MartinMolestina <martin.molestina@gmail.com>
2025-06-09 14:55:19 -0500

    tkinter root and canva started

---
+++ window.py
@@ -0,0 +1,28 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+
+    def __init__(self, width, height):
+
+        self.root = Tk()
+        self.root.title("Maze Generator")
+
+        self.canvas = Canvas(self.root, width=width, height=height)
+        self.canvas.pack(expand=True, fill=BOTH)
+
+        self.running = False
+        self.root.protocol("WM_DELETE_WINDOW",self.close)
+
+    def redraw(self):
+        self.root.update_idletasks()
+        self.root.update()
+
+
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+
+    def close(self):
+        self.running = False
+
