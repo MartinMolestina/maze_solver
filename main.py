659df7a76967d710374f0ae1abd62a81f0b996e6
MartinMolestina <martin.molestina@gmail.com>
2025-06-10 16:35:53 -0500

    line and draw methods tested in main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from window import Window
+from window import Window, Line, Point
 
 def main():
     win = Window(800, 600)
-    win.wait_for_close()
+    p1 = Point(50, 50)
+    p2 = Point(750, 750)
+    diagonal_line = Line(p1, p2)
 
+    win.draw_line(diagonal_line, "green")
+    win.wait_for_close()
 
 if __name__ == "__main__":
     main()
--- window.py
+++ window.py
@@ -25,4 +25,28 @@ class Window:
 
     def close(self):
         self.running = False
+    
+    def draw_line(self, line, color):
+        line.draw(self.canvas, color)
 
+
+
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, color):
+        canvas.create_line(
+                self.p1.x, self.p1.y,
+                self.p2.x, self.p2.y,
+                fill=color,
+                width=2)
